754259e2194004638b884bd4c42b6a205f344ce1
lukehongg <lukehongg@handong.ac.kr>
2022-12-29 22:13:43 +0900

    example

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App">This is Daangun Clone</div>;
+  return <div className="App">Thidsadsaasdasdasdasdasdasds is Daangun Clone</div>;
 }
 
 export default App;
