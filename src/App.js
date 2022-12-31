0ee718429ba00ba3e9a71019ba9322e65352d3bb
lukehongg <lukehongg@handong.ac.kr>
2022-12-31 22:25:56 +0900

    [#3]: Router Setting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { SnackbarProvider } from 'notistack';
+import Router from './Router';
+import { theme } from './theme/theme';
 
 function App() {
-  return <div className="App">Thidsadsaasdasdasdasdasdasds is Daangun Clone</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <SnackbarProvider maxSnack={3} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}>
+        <CssBaseline />
+        <Router />
+      </SnackbarProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/pages/Page2.js
@@ -0,0 +1,8 @@
+function Page2() {
+  return (
+    <div>
+      <h2>Page two</h2>
+    </div>
+  );
+}
+export default Page2;
