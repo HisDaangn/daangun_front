368d2efae72d19746ecf40669ff5b20a5d8662fa
lukehongg <lukehongg@handong.ac.kr>
2023-01-03 22:57:43 +0900

    [#6]: chat init

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Page1 from './pages/Page1';
 import Page2 from './pages/Page2';
+import WebSocketTest from './pages/WebSocket';
 function Router() {
   return (
     <BrowserRouter>
@@ -11,6 +12,7 @@ function Router() {
           <Route path="/home" element={<Home />} />
           <Route path="/page1" element={<Page1 />} />
           <Route path="/page2" element={<Page2 />} />
+          <Route path="/websockettest" element={<WebSocketTest />} />
         </>
       </Routes>
     </BrowserRouter>
