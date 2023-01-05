89030d8456be6fd3d19edf90f00a6e90ce63434f
lukehongg <lukehongg@handong.ac.kr>
2023-01-05 20:51:14 +0900

    [#11]: Header 연결

---
+++ src/components/common/layout/Header.js
@@ -0,0 +1,22 @@
+import { Container } from '@mui/system';
+import { AppBar, Box, Toolbar, Button } from '@mui/material';
+
+import React from 'react';
+
+export default function Header() {
+  return (
+    <AppBar
+      position="fixed"
+      color="inherit"
+      sx={{
+        width: '100%',
+        boxShadow: 0,
+      }}
+    >
+      <Toolbar sx={{ borderBottom: '1px solid', borderColor: 'divider' }}>
+        <Box sx={{ flexGrow: 1 }} />
+        <Button>로그인</Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
