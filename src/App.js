0328ecb2cf75b0d9a9bd642fc93eca6b3f20033c
lukehongg <79990740+lukehongg@users.noreply.github.com>
2023-01-05 20:51:48 +0900

    [#11]: Header 연결 (#12)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider, Box, Paper } from '@mui/material';
 import { SnackbarProvider } from 'notistack';
 import Router from './Router';
 import { theme } from './theme/theme';
-
+import Header from './components/common/layout/Header';
+import Footer from './components/common/layout/Footer';
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <SnackbarProvider maxSnack={3} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}>
         <CssBaseline />
-        <Router />
+        <Header />
+        <Box
+          sx={{
+            display: 'flex',
+
+            flexWrap: 'wrap',
+            '& > :not(style)': {
+              pt: 3,
+              mt: 1,
+              width: '100%',
+              height: 'auto',
+            },
+          }}
+        >
+          <Paper elevation={0}>
+            <Router />
+          </Paper>
+        </Box>
+        {/* <Footer /> */}
       </SnackbarProvider>
     </ThemeProvider>
   );
--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -1,13 +1,15 @@
 import Chatroom from './Chatroom';
+import But from '../../components/common/Button';
+import { Button, Container, useTheme, Box } from '@mui/material';
 
 function Chat() {
+  const theme = useTheme();
   return (
-    <>
-      <div>
-        <h1>Chat</h1>
-        <Chatroom />
-      </div>
-    </>
+    <Container>
+      <Box display="flex" justifyContent={'center'}>
+        <But />
+      </Box>
+    </Container>
   );
 }
 export default Chat;
