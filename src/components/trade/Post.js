8f757d80af1ea356206df458b419d8d5907ecadc
uojin29 <uojin29@gmail.com>
2023-01-08 06:18:45 +0900

    상세보기 거의 완성

--- src/components/trade/Post.js
+++ src/components/trade/Post.js
@@ -4,6 +4,7 @@ const Post = [
         title: "라면",
         imgs: "https://img.danawa.com/prod_img/500000/489/206/img/10206489_1.jpg?shrink=330:330&_v=20200714161025",
         price: "1,500원",
+        content: "새벽에 올려봅니다~"
     },
     {
         id: 1,
