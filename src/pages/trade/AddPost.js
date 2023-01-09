61862960d3aabc43b122c53d9e85f15d0b929b34
uojin29 <108047119+uojin29@users.noreply.github.com>
2023-01-09 17:33:30 +0900

    Feat/trade/#15 (#24)
    * Make TradeBoard
    
    * 상세보기 거의 완성
    
    * make a DeleteModal.css
    
    * 올라가라. ..
    
    * make AddPost, EditPost, AddPost.css
    
    * change AddPost, EditPost, delete AddPost.css
    
    * ssss

--- src/pages/trade/AddPost.js
+++ src/pages/trade/AddPost.js
@@ -1,4 +1,3 @@
-import './AddPost.css'
 import React from 'react';
 import ClearIcon from '@mui/icons-material/Clear';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
@@ -8,8 +7,13 @@ import {
   ThemeProvider,
   Button,
   Grid,
+  Divider,
+  TextField,
+  Input,
+  Checkbox, FormControlLabel,
 } from "@mui/material";
 function AddPost() {
+  const ariaLabel = { 'aria-label': 'description' };
   const theme = createTheme({
     palette: {
       primary: {
@@ -22,39 +26,35 @@ function AddPost() {
   });
   return (
     <>
-      <div>
-        <Grid container spacing={3}>
-          <ThemeProvider theme={theme}>
-            <Button startIcon={<ClearIcon />}></Button>
-            <Box sx={{ fontSize: 20, fontWeight: 'bold', m: 1 }}>중고거래 글쓰기</Box>
-            <Button color="secondary">완료</Button>
-          </ThemeProvider>
-        </Grid>
-      </div>
-      <hr />
-      <div>
-        <Grid>
-          <ThemeProvider theme={theme}>
-            <Button startIcon={<CameraAltIcon />}>0/10</Button>
-          </ThemeProvider>
-        </Grid>
-      </div>
-      <hr />
-      <div>
-        <Grid>
-          <input placeholder="글제목"></input>
-        </Grid>
-      </div>
-      <hr />
-      <div>
-        <input placeholder="가격"></input>
-        <input type="checkbox" id="share" name="share" value="나눔" />
-        <label htmlFor="share">나눔</label>
-      </div>
-      <hr />
-      <div className="contents">
-        <textarea placeholder="게시글 내용을 작성해주세요. (판매 금지 물품은 게시가 제한될 수 있어요.)"></textarea>
-      </div>
+      <br />
+      <Grid container spacing={3}>
+        <ThemeProvider theme={theme}>
+          <Button startIcon={<ClearIcon />}></Button>
+          <Box sx={{ fontSize: 20, fontWeight: 'bold', m: 1 }}>중고거래 글쓰기</Box>
+          <Button color="secondary">완료</Button>
+        </ThemeProvider>
+      </Grid>
+      <Divider />
+      <Grid>
+        <ThemeProvider theme={theme}>
+          <Button variant="outlined" startIcon={<CameraAltIcon />}>0/10</Button>
+        </ThemeProvider>
+      </Grid>
+      <Divider />
+      <Grid>
+        <Input placeholder="글제목" inputProps={ariaLabel} />
+      </Grid>
+      <Divider />
+      <Input placeholder="가격" inputProps={ariaLabel} />
+      <FormControlLabel control={<Checkbox defaultChecked />} label="나눔" />
+      <Divider />
+      <TextField
+        id="standard-multiline-static"
+        multiline
+        rows={6}
+        placeholder="게시글 내용을 작성해주세요. (판매 금지 물품은 게시가 제한될 수 있어요.)"
+        variant="standard"
+      />
     </>
   );
 }
