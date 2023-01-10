d5d043d935d02831e2451d2dc2fba7a96626c61f
SeokminBang <22001024@handong.ac.kr>
2023-01-11 00:13:58 +0900

    [#5 : 노력중..

--- src/App.js
+++ src/App.js
@@ -5,36 +5,36 @@ import { theme } from "./theme/theme";
 import Header from "./components/common/layout/Header";
 import Footer from "./components/common/layout/Footer";
 function App() {
-	return (
-		<ThemeProvider theme={theme}>
-			<SnackbarProvider
-				maxSnack={3}
-				anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-			>
-				<CssBaseline />
-				<Header />
-				<Box
-					sx={{
-						display: "flex",
-						flexWrap: "wrap",
-						"& > :not(style)": {
-							pt: 3,
-							mt: 0,
-							width: "100%",
-							height: "auto",
-						},
-						padding: 0,
-						margin: 0,
-					}}
-				>
-					<Paper elevation={0}>
-						<Router />
-					</Paper>
-				</Box>
-				{/* <Footer /> */}
-			</SnackbarProvider>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <SnackbarProvider
+        maxSnack={3}
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+      >
+        <CssBaseline />
+        {/* <Header /> */}
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            "& > :not(style)": {
+              pt: 3,
+              mt: 0,
+              width: "100%",
+              height: "auto",
+            },
+            padding: 0,
+            margin: 0,
+          }}
+        >
+          <Paper elevation={0}>
+            <Router />
+          </Paper>
+        </Box>
+        {/* <Footer /> */}
+      </SnackbarProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/Router.js
+++ src/Router.js
@@ -8,39 +8,43 @@ import EditPost from "./pages/trade/EditPost";
 import TradeBoard from "./pages/trade/TradeBoard";
 import ViewPost from "./pages/trade/ViewPost";
 import { drawerWidth } from "./constants/commons";
+import Header from "./components/common/layout/Header";
+import Mypage from "./components/user/Mypage";
 
 function Router() {
-	return (
-		<BrowserRouter>
-			<Box display="flex">
-				<Box
-					component="main"
-					sx={{
-						flexGrow: 1,
-						p: 0,
-						pt: 5,
-						pb: 0,
-						height: "100vh",
-						width: `calc(100% - ${drawerWidth}px)`,
-					}}
-				>
-					<Routes>
-						<>
-							<Route path="/" element={<Navigate to="/home" />} />
-							<Route path="/home" element={<Home />} />
-							<Route path="/chat/room/:id" element={<Chat />} />
-							<Route path="/chat" element={<Chat />} />
-							<Route path="/addpost" element={<AddPost />} />
-							<Route path="/editpost" element={<EditPost />} />
-							<Route path="/tradeboard" element={<TradeBoard />} />
-							<Route path="/viewpost" element={<ViewPost />} />
-							<Route path="/websockettest" element={<WebSocketTest />} />
-						</>
-					</Routes>
-				</Box>
-			</Box>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Header />
+      <Box display="flex">
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            p: 0,
+            pt: 5,
+            pb: 0,
+            height: "100vh",
+            width: `calc(100% - ${drawerWidth}px)`,
+          }}
+        >
+          <Routes>
+            <>
+              <Route path="/" element={<Navigate to="/home" />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/chat/room/:id" element={<Chat />} />
+              <Route path="/chat" element={<Chat />} />
+              <Route path="/addpost" element={<AddPost />} />
+              <Route path="/editpost" element={<EditPost />} />
+              <Route path="/tradeboard" element={<TradeBoard />} />
+              <Route path="/viewpost" element={<ViewPost />} />
+              <Route path="/mypage" element={<Mypage />} />
+              <Route path="/websockettest" element={<WebSocketTest />} />
+            </>
+          </Routes>
+        </Box>
+      </Box>
+    </BrowserRouter>
+  );
 }
 
 export default Router;
