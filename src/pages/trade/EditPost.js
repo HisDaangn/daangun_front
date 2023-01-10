7ba72d9df12bb7275189dfa29bc156875237013e
uojin29 <108047119+uojin29@users.noreply.github.com>
2023-01-10 14:12:29 +0900

    Feat/trade/#15 (#33)
    * Make TradeBoard
    
    * 상세보기 거의 완성
    
    * make a DeleteModal.css
    
    * 올라가라. ..
    
    * make AddPost, EditPost, AddPost.css
    
    * change AddPost, EditPost, delete AddPost.css
    
    * ssss

--- src/pages/trade/EditPost.js
+++ src/pages/trade/EditPost.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ClearIcon from '@mui/icons-material/Clear';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import "./EditModal.css";
 import {
   Box,
   createTheme,
@@ -12,7 +12,8 @@ import {
   Input,
   Checkbox, FormControlLabel,
 } from "@mui/material";
-function EditPost() {
+const EditPost = (props) => {
+  const { open, close } = props;
   const ariaLabel = { 'aria-label': 'description' };
   const theme = createTheme({
     palette: {
@@ -25,37 +26,52 @@ function EditPost() {
     },
   });
   return (
-    <>
+    <><div className={open ? 'openModal modal' : 'modal'}>
+      {open ? (
+        <section>
+          <header>
+            <ThemeProvider theme={theme}>
+              <Box sx={{ fontSize: 20, fontWeight: 'bold', m: 1 }}>중고거래 글 수정하기</Box>
+            </ThemeProvider>
+            <button className="close" onClick={close}>
+              &times;
+            </button>
+          </header>
+          <main>
+            <ThemeProvider theme={theme}>
+              <Button variant="outlined" startIcon={<CameraAltIcon />}>0/10</Button>
+            </ThemeProvider>
+            <Divider />
+            <Grid>
+              <Input placeholder="글제목" inputProps={ariaLabel} />
+            </Grid>
+            <Divider />
+            <Input placeholder="가격" inputProps={ariaLabel} />
+            <FormControlLabel control={<Checkbox defaultChecked />} label="나눔" />
+            <Divider />
+            <TextField
+              id="standard-multiline-static"
+              multiline
+              rows={6}
+              placeholder="게시글 내용을 작성해주세요. (판매 금지 물품은 게시가 제한될 수 있어요.)"
+              variant="standard"
+            />
+          </main>
+          <footer>
+            <button className="close" onClick={close}>
+              저장하기
+            </button>
+          </footer>
+        </section>
+      ) : null}
+    </div>
+      <br /><br /><br /><br />
 
-      <br />
-      <Grid container spacing={3}>
-        <ThemeProvider theme={theme}>
-          <Button startIcon={<ClearIcon />}></Button>
-          <Box sx={{ fontSize: 20, fontWeight: 'bold', m: 1 }}>중고거래 글 수정하기</Box>
-          <Button color="secondary">완료</Button>
-        </ThemeProvider>
-      </Grid>
-      <Divider />
-      <Grid>
-        <ThemeProvider theme={theme}>
-          <Button variant="outlined" startIcon={<CameraAltIcon />}>0/10</Button>
-        </ThemeProvider>
-      </Grid>
-      <Divider />
-      <Grid>
-        <Input placeholder="글제목" inputProps={ariaLabel} />
-      </Grid>
-      <Divider />
-      <Input placeholder="가격" inputProps={ariaLabel} />
-      <FormControlLabel control={<Checkbox defaultChecked />} label="나눔" />
-      <Divider />
-      <TextField
-        id="standard-multiline-static"
-        multiline
-        rows={6}
-        placeholder="게시글 내용을 작성해주세요. (판매 금지 물품은 게시가 제한될 수 있어요.)"
-        variant="standard"
-      />
+
+
+
+
+
     </>
   );
 }
