95bd5f52a489b7047513952166eab68c396b34c6
uojin29 <108047119+uojin29@users.noreply.github.com>
2023-01-15 23:17:59 +0900

    Feat/trade/#15 (#44)
    * Make TradeBoard
    
    * 상세보기 거의 완성
    
    * make a DeleteModal.css
    
    * 올라가라. ..
    
    * make AddPost, EditPost, AddPost.css
    
    * change AddPost, EditPost, delete AddPost.css
    
    * ssss
    
    * make ImageUpload
    
    * Image Upload 됨
    
    * PostDetail 수정 중
    
    * image upload 됨

--- src/components/trade/PostDetail.js
+++ src/components/trade/PostDetail.js
@@ -5,11 +5,8 @@ import {
     Avatar,
     Box,
     Slider,
-    Button,
-    Grid,
+    Stack,
 } from "@mui/material";
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 const PostDetail = (props) => {
     const [deleteModalOpen, setDeleteModalOpen] = useState(false);
 
@@ -37,46 +34,50 @@ const PostDetail = (props) => {
         marginLeft: "10px",
     }
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={3}>
-                <Button startIcon={<ArrowBackIosIcon />}></Button>
-                <Box
-                    component="img"
-                    sx={{
-                        height: 400,
-                        width: 400,
-                    }}
-                    alt="The house from the offer."
-                    src="https://img.danawa.com/prod_img/500000/489/206/img/10206489_1.jpg?shrink=330:330&_v=20200714161025"
-                />
-                <Button startIcon={<ArrowForwardIosIcon />}></Button>
-            </Grid>
-            <Box>
-                <Grid container spacing={3}>
-                    <Avatar src="https://cdn-icons-png.flaticon.com/512/1946/1946429.png" />
-                    <Box sx={{ fontSize: 16, fontWeight: 'regiar' }}>username <br /> 포항시 흥해읍</Box>
-                    <Grid item xs={7}>
+        <div style={{
+            position: "relative",
+            width: "100%",
+            height: "100%",
+        }}>
+            <div style={{
+                position: "absolute",
+                left: "50%",
+                top: "50%",
+                width: "60%",
+                height: "70%",
+                transform: "translate(-50%, -50%)",
+                // background: "black",
+            }}>
+                <img style={{
+                    width: "100%",
+                    height: "400px",
+                }} src="https://img.danawa.com/prod_img/500000/489/206/img/10206489_1.jpg?shrink=330:330&_v=20200714161025" alt='img' />
+
+                <div >
+                    <Stack display="flex" direction="row" spacing={5} justifyContent="center">
+                        <Avatar src="https://cdn-icons-png.flaticon.com/512/1946/1946429.png" />
+                        <Box sx={{ fontSize: 16, fontWeight: 'regiar' }}>username <br /> 포항시 흥해읍</Box>
                         <button style={BtnStyle} onClick={openEditModal}>수정하기</button>
                         <EditModal open={editModalOpen} close={closeEditModal}></EditModal>
                         <button style={BtnStyle} onClick={openDeleteModal}>삭제하기</button>
                         <DeleteModal open={deleteModalOpen} close={closeDeleteModal} header="Modal heading">
                             게시물이 삭제됩니다!
                         </DeleteModal>
-                    </Grid>
-                    <Grid>
-                        <Slider width={100} defaultValue={36.5} aria-label="Default" valueLabelDisplay="auto" />
+                        <Slider disabled sx={{ width: "150px" }} defaultValue={36.5} aria-label="Default" valueLabelDisplay="auto" />
                         <Avatar src="https://d1unjqcospf8gs.cloudfront.net/assets/home/articles/face-icon-set@2x-0bece009c619b4706f52a750aca82448334aa3e39d353579f2ce9c365639a03b.png" />
-                    </Grid>
-                </Grid>
+                    </Stack>
+                </div>
 
-            </Box>
-            <hr />
-            <div>
-                <Box sx={{ fontSize: 20, fontWeight: 'bold', m: 1 }}>라면 사실 분~</Box>
-                <Box sx={{ fontSize: 16, fontWeight: 'bold', m: 1 }}>1,500원</Box>
-                <Box sx={{ fontSize: 16, fontWeight: 'regular', m: 1 }}>새벽이라서 팔아봅니다~</Box>
+
+                <hr />
+                <div>
+                    <Box sx={{ fontSize: 20, fontWeight: 'bold', m: 2 }}>라면 사실 분~</Box>
+                    <Box sx={{ fontSize: 16, fontWeight: 'bold', m: 2 }}>1,500원</Box>
+                    <Box sx={{ fontSize: 16, fontWeight: 'regular', m: 2 }}>새벽이라서 팔아봅니다~</Box>
+                </div>
+
             </div>
-        </Box>
+        </div >
     )
 }
 export default PostDetail;
