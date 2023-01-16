9d26c9aad31e28cd62f3c0284209573bec8808e1
uojin29 <108047119+uojin29@users.noreply.github.com>
2023-01-17 06:10:42 +0900

    Feat/trade/#60 (#61)
    * 오류 해결
    
    * [#60]:이슈를 새로 만듦

--- src/components/trade/PostDetail.js
+++ src/components/trade/PostDetail.js
@@ -8,13 +8,15 @@ import good from "./img/good.png";
 import verygood from "./img/verygood.png";
 import excellent from "./img/excellent.png";
 
-import  './Cards.css';
+import './Cards.css';
 import {
     Avatar,
     Box,
     Slider,
     Stack,
-    createTheme, ThemeProvider
+    createTheme,
+    ThemeProvider,
+    Modal,
 } from "@mui/material";
 const PostDetail = (props) => {
     const { postID } = useParams();
@@ -22,7 +24,10 @@ const PostDetail = (props) => {
     async function getData() {
         try {
             //응답 성공
-            const response = await axios.get(`http://localhost:8080/trade/${postID}`);
+            const response = await axios.get(`http://localhost:8080/trade/${postID}`)
+                .then(response => {
+                    setValue(response.data);
+                });
             console.log(response);
         } catch (error) {
             //응답 실패
@@ -32,12 +37,14 @@ const PostDetail = (props) => {
     useEffect(() => {
         getData();
     }, [])
+    const [value, setValue] = useState([]);
     //PATCH 끌어올리기
-    async function lift({ expose_at }) {
+    async function lift() {
         try {
             //응답 성공
             const response = await axios.patch(`http://localhost:8080/trade/lift/${postID}`, {
-                expose_at: `${expose_at}`,
+                expose_at: `${value.expose_at}`,
+
             });
             console.log(response);
             console.log("liftBtn click !");
@@ -47,6 +54,7 @@ const PostDetail = (props) => {
         }
     }
     const temperature = 36.7;
+
     // const [temperature, setTemperature] = useState();
     const theme = createTheme({
         palette: {
@@ -121,17 +129,51 @@ const PostDetail = (props) => {
         border: "none",
         marginLeft: "10px",
     }
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
     return (
-        <div>
-            <div className="detail-container">
-                <img className="detail-img" src= "/img/Ramen.webp" alt='img' />
-                <div className="detail-profile">
+        <div style={{
+            position: "relative",
+            width: "100%",
+            height: "100%",
+        }}>
+            <div style={{
+                position: "absolute",
+                left: "50%",
+                top: "50%",
+                width: "60%",
+                height: "70%",
+                transform: "translate(-50%, -50%)",
+            }}>
+                <img style={{
+                    width: "100%",
+                    height: "400px",
+                }} src="https://www.shutterstock.com/image-photo/korean-spicy-instant-noodles-egg-260nw-1296771487.jpg" alt='img' />
+
+                <div >
+
                     <Stack direction="row" spacing={5} justifyContent="center">
-                    
+
                         <Avatar src="https://cdn-icons-png.flaticon.com/512/1946/1946429.png" />
                         <Box sx={{ fontSize: 20, fontWeight: 'regiar' }}>username</Box>
                         <button style={BtnStyle} onClick={openEditModal}>수정하기</button>
-                        <EditModal open={editModalOpen} close={closeEditModal}></EditModal>
+                        <Modal
+                            open={editModalOpen}
+                            onClose={closeEditModal}
+                        >
+                            <Box sx={style}>
+                                <EditModal {...value} />
+                            </Box>
+                        </Modal>
                         <button style={BtnStyle} onClick={openDeleteModal}>삭제하기</button>
                         <DeleteModal open={deleteModalOpen} close={closeDeleteModal} header="Modal heading">
                             게시물이 삭제됩니다!
@@ -143,10 +185,11 @@ const PostDetail = (props) => {
                     </Stack>
                 </div>
                 <hr />
-                <div className="detail-content">
-                    <Box sx={{ fontSize: 20, fontWeight: 'bold', m: 2 }}>라면 사실 분~</Box>
-                    <Box sx={{ fontSize: 16, fontWeight: 'bold', m: 2 }}>1,500원</Box>
-                    <Box sx={{ fontSize: 16, fontWeight: 'regular', m: 2 }}>새벽이라서 팔아봅니다~</Box>
+                <div>
+                    <Box sx={{ fontSize: 20, fontWeight: 'bold', m: 2 }}>{value.title}</Box>
+                    <Box sx={{ fontSize: 16, fontWeight: 'bold', m: 2 }}>{value.price}원</Box>
+                    <Box sx={{ fontSize: 16, fontWeight: 'regular', m: 2 }}>{value.content}</Box>
+
                 </div>
                 <Stack direction="row" justifyContent="flex-end">
                     <Link to={"/chat"} style={{ textDecoration: "none" }}>
