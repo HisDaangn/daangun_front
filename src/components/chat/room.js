17625dd8609c715ef0a531572f3ace0784ff83c6
Hyeongi Kim <61077215+hgkim00@users.noreply.github.com>
2023-01-17 20:30:18 +0900

    Feat/chat/#10 (#62)
    * axios chat existRoom
    
    * commit
    
    * commit

--- src/components/chat/room.js
+++ src/components/chat/room.js
@@ -7,28 +7,28 @@ import { Link } from "react-router-dom";
 
 function Room({ room }) {
 	return (
-		<Link to={`/chat/room/${room.id}`} style={{ textDecoration: "none" }}>
+		<Link to={`/chat/room/${room.roomId}`} style={{ textDecoration: "none" }}>
 			<ListItemButton
-				key={room.id}
+				key={room.roomId}
 				sx={{ borderBottom: "1px solid", borderColor: "gainsboro" }}
 			>
 				<ListItemAvatar>
 					<Avatar
 						alt="default"
-						src={room.profileImg}
+						src="/img/profile_default.png" //임시로 이렇게 지정.
 						sx={{ width: 40, height: 40 }}
 					/>
 				</ListItemAvatar>
 				<ListItemText
-					primary={room.chatUser}
-					secondary={room.lastMessage}
+					primary={room.pubName}
+					secondary={room.post.content}
 					style={{
 						color: "black",
 					}}
 				/>
 				<Box
 					component={"img"}
-					src={room.itemImg}
+					src={room.photoURL}
 					width={"40px"}
 					// height={"684px"}
 				/>
--- src/pages/chat/Chatroom.js
+++ src/pages/chat/Chatroom.js
@@ -1,11 +1,11 @@
 import EmptyRoom from "../../components/chat/emptyRoom";
 import ExistRoom from "../../components/chat/existRoom";
 
-function Chatroom({ roomId }) {
+function Chatroom({ roomId, chatRoom }) {
 	if (typeof roomId === "undefined") {
 		return <EmptyRoom />;
 	} else {
-		return <ExistRoom id={roomId} />;
+		return <ExistRoom roomId={roomId} />;
 	}
 }
 export default Chatroom;
