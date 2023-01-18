d2b4b4b8918057e0841eaab655dfc0025a177c24
hgkim00 <21900215@handong.ac.kr>
2023-01-18 18:16:59 +0900

    merge dev

--- src/components/trade/PostDetail.js
+++ src/components/trade/PostDetail.js
@@ -21,7 +21,6 @@ import {
 import { useNavigate } from "react-router-dom";
 
 const PostDetail = (props) => {
-<<<<<<< HEAD
 	const { postID } = useParams();
 	const [color, setColor] = useState("#1561a9");
 	const [img, setImg] = useState();
