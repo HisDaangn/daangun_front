48767645e6664ea69c41aaf1fe733d33df75913b
uojin29 <uojin29@gmail.com>
2023-01-18 18:55:35 +0900

    postdetail 꼬인 거 다시 수정함

--- src/components/trade/PostDetail.js
+++ src/components/trade/PostDetail.js
@@ -22,17 +22,63 @@ import { useNavigate } from "react-router-dom";
 
 const PostDetail = (props) => {
     const { postID } = useParams();
+    const [value, setValue] = useState([]);
+    const [writer, setWriter] = useState();
+    const [init, setInit] = useState(false);
     const [color, setColor] = useState("#1561a9");
     const [img, setImg] = useState();
-    const [value, setValue] = useState([]);
+    // const [temperature, setTemperature] = useState();
     const [deleteModalOpen, setDeleteModalOpen] = useState(false);
     const [editModalOpen, setEditModalOpen] = useState(false);
     const [chatRoomId, setChatRoomId] = useState("");
     const navigate = useNavigate();
 
+    const del = async () => {
+        console.log("del 실행");
+        // await deleteData();
+        closeDeleteModal();
+    };
     useEffect(() => {
         getData();
-    }, []);
+    }, [])
+    useEffect(() => {
+        if (init) setWriter(value.writer)
+    }, [init])
+    useEffect(() => {
+        if (init) {
+            if (writer.temperature < 36.5) {
+                setColor("bad");
+                setImg(bad);
+            } else if (writer.temperature < 40) {
+                setColor("#319e45");
+                setImg(good);
+            } else if (writer.temperature < 50) {
+                setColor("#df9100");
+                setImg(verygood);
+            } else {
+                setColor("#de5d06");
+                setImg(excellent);
+            }
+        }
+
+    }, [init]);
+    const theme = createTheme({
+        palette: {
+            bad: {
+                main: '#1561a9',
+            },
+            good: {
+                main: '#319e45',
+            },
+            verygood: {
+                main: '#df9100',
+            },
+            excelent: {
+                main: '#de5d06',
+            },
+        },
+    });
+
     useEffect(
         function () {
             if (chatRoomId !== "") {
@@ -46,23 +92,6 @@ const PostDetail = (props) => {
         [chatRoomId]
     );
 
-    const temperature = 36.7;
-
-    useEffect(() => {
-        if (temperature < 36.5) {
-            setColor("bad");
-            setImg(bad);
-        } else if (temperature < 40) {
-            setColor("#319e45");
-            setImg(good);
-        } else if (temperature < 50) {
-            setColor("#df9100");
-            setImg(verygood);
-        } else {
-            setColor("#de5d06");
-            setImg(excellent);
-        }
-    }, [temperature]);
     // POST
     async function moveToChatRoom() {
         try {
@@ -82,12 +111,12 @@ const PostDetail = (props) => {
     async function getData() {
         try {
             //응답 성공
-            const response = await axios
-                .get(`http://localhost:8080/trade/${postID}`)
+            await axios.get(`http://localhost:8080/trade/${postID}`)
                 .then((response) => {
                     setValue(response.data);
+                    setWriter(response.data.writer);
                 });
-            console.log(response);
+            setInit(true);
         } catch (error) {
             //응답 실패
             console.error(error);
@@ -97,12 +126,10 @@ const PostDetail = (props) => {
     async function lift() {
         try {
             //응답 성공
-            const response = await axios.patch(
-                `http://localhost:8080/trade/lift/${postID}`,
-                {
-                    expose_at: `${value.expose_at}`,
-                }
-            );
+            const response = await axios.patch(`http://localhost:8080/trade/lift/${postID}`, {
+                expose_at: `${value.expose_at}`,
+
+            });
             console.log(response);
             console.log("liftBtn click !");
         } catch (error) {
@@ -110,53 +137,43 @@ const PostDetail = (props) => {
             console.error(error);
         }
     }
-
-    // const [temperature, setTemperature] = useState();
-    const theme = createTheme({
-        palette: {
-            bad: {
-                main: "#1561a9",
-            },
-            good: {
-                main: "#319e45",
-            },
-            verygood: {
-                main: "#df9100",
-            },
-            excelent: {
-                main: "#de5d06",
-            },
-        },
-    });
-
+    //DELETE 삭제하기
+    async function deleteData() {
+        // console.log(id.id);
+        try {
+            //응답 성공
+            const response = await axios.delete(`http://localhost:8080/trade/${postID}`);
+            console.log(response);
+            console.log(postID);
+        } catch (error) {
+            //응답 실패
+            console.error(error);
+        }
+    }
     const openDeleteModal = () => {
         setDeleteModalOpen(true);
     };
     const closeDeleteModal = () => {
         setDeleteModalOpen(false);
     };
+
+
     const openEditModal = () => {
         setEditModalOpen(true);
     };
     const closeEditModal = () => {
         setEditModalOpen(false);
     };
-
-    const onChat = (chatBtn) => {
+    const onChat = chatBtn => {
         console.log("chatBtn click !");
-        moveToChatRoom();
-    };
-
-    const onLift = (liftBtn) => {
-        console.log("liftBtn click !");
-    };
+    }
     const BtnStyle = {
         border: "1px solid #d1d3d8",
         padding: "5px 15px",
         color: "#212124",
         fontWeight: "bolder",
         fontSize: "13px",
-    };
+    }
     const staticBtnStyle = {
         color: "white",
         backgroundColor: "#ed7833",
@@ -167,15 +184,27 @@ const PostDetail = (props) => {
         height: "30px",
         border: "none",
         marginLeft: "10px",
-    };
+    }
+    const delBtn = {
+        color: "white",
+        backgroundColor: "#ed7833",
+        borderRadius: "3px",
+        fontWeight: "bold",
+        fontSize: "10px",
+        width: "80px",
+        height: "30px",
+        marginLeft: "10px",
+        border: "none",
+        float: "right",
+    }
     const style = {
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
         width: 400,
-        bgcolor: "background.paper",
-        border: "2px solid #000",
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
         boxShadow: 24,
         p: 4,
     };
@@ -209,48 +238,44 @@ const PostDetail = (props) => {
                 <div>
                     <Stack direction="row" spacing={5} justifyContent="center">
                         <Avatar src="https://cdn-icons-png.flaticon.com/512/1946/1946429.png" />
-                        <Box sx={{ fontSize: 20, fontWeight: "regiar" }}>username</Box>
-                        <button style={BtnStyle} onClick={openEditModal}>
-                            수정하기
-                        </button>
-                        <Modal open={editModalOpen} onClose={closeEditModal}>
+                        <Box sx={{ fontSize: 20, fontWeight: 'regiar' }}>{init ? writer.name : "username"}</Box>
+                        <button style={BtnStyle} onClick={openEditModal}>수정하기</button>
+                        <Modal
+                            open={editModalOpen}
+                            onClose={closeEditModal}
+                        >
                             <Box sx={style}>
-                                <EditModal {...value} />
+                                <EditModal id={postID} photoURL={value.photoURL} title={value.title} price={value.price} content={value.content} />
                             </Box>
                         </Modal>
-
-                        <Modal open={deleteModalOpen} onClose={closeDeleteModal} >
-                            <DeleteModal >
-                                <button style={BtnStyle} onClick={openDeleteModal}>
-                                    삭제하기
+                        <button style={BtnStyle} onClick={openDeleteModal}>삭제하기</button>
+                        <Modal
+                            open={deleteModalOpen}
+                            onClose={closeDeleteModal}
+                        >
+                            <Box sx={style}>
+                                <DeleteModal />
+                                <br />
+                                <button style={delBtn} onClick={del}>
+                                    확인
                                 </button>
-                                게시물이 삭제됩니다!
-                            </DeleteModal>
+                                <button style={delBtn} onClick={closeDeleteModal}>
+                                    취소
+                                </button>
+                            </Box>
                         </Modal>
-
                         <ThemeProvider theme={theme}>
-                            <Slider
-                                color="good"
-                                sx={{ width: "150px" }}
-                                value={temperature}
-                                aria-label="Default"
-                                valueLabelDisplay="auto"
-                            />
+                            <Slider color='good' sx={{ width: "150px" }} value={init ? writer.temperature : 36.5} aria-label="Default" valueLabelDisplay="auto" />
                         </ThemeProvider>
                         <Avatar alt="img" src={img} />
                     </Stack>
                 </div>
                 <hr />
                 <div>
-                    <Box sx={{ fontSize: 20, fontWeight: "bold", m: 2 }}>
-                        {value.title}
-                    </Box>
-                    <Box sx={{ fontSize: 16, fontWeight: "bold", m: 2 }}>
-                        {value.price}원
-                    </Box>
-                    <Box sx={{ fontSize: 16, fontWeight: "regular", m: 2 }}>
-                        {value.content}
-                    </Box>
+                    <Box sx={{ fontSize: 20, fontWeight: 'bold', m: 2 }}>{value.title}</Box>
+                    <Box sx={{ fontSize: 16, fontWeight: 'bold', m: 2 }}>{value.price} 원</Box>
+                    <Box sx={{ fontSize: 16, fontWeight: 'regular', m: 2 }}>{value.content}</Box>
+
                 </div>
                 <Stack direction="row" justifyContent="flex-end">
                     <button style={staticBtnStyle} onClick={onChat}>
